f31b0bf8c8c712f15b75ebd369c72003d7afcdda
IBNOU ABASS WADE [SNT DSI/INS] <TMP_WADE62890@orange-sonatel.com>
2023-01-03 12:30:44 +0000

    mon deusieme commit

--- src/main/java/com/example/zeynashop/service/ProduitServiceImpl.java
+++ src/main/java/com/example/zeynashop/service/ProduitServiceImpl.java
@@ -1,5 +1,6 @@
 package com.example.zeynashop.service;
 
+import com.example.zeynashop.entities.Categorie;
 import com.example.zeynashop.entities.Produit;
 import com.example.zeynashop.repos.ProduitRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,38 +14,67 @@ public class ProduitServiceImpl implements ProduitService{
 
 	@Override
 	public Produit saveProduit(Produit p) {
-
-		return produitRepository.save(p);
+		return null;
 	}
 
 	@Override
-	public Produit updateProduit(Produit p)
-	{
-		return produitRepository.save(p);
+	public Produit updateProduit(Produit p) {
+		return null;
 	}
 
 	@Override
 	public void deleteProduit(Produit p) {
-		produitRepository.delete(p);
 
 	}
 
 	@Override
 	public void deleteProduitById(Long id) {
-		produitRepository.deleteById(id);
 
 	}
 
 	@Override
-	public Produit getProduit(Long id)
-	{
-		return produitRepository.findById(id).get();
+	public Produit getProduit(Long id) {
+		return null;
 	}
 
 	@Override
 	public List<Produit> getAllProduits() {
-
-		return produitRepository.findAll();
+		return null;
 	}
+
+	@Override
+	public List<Produit> findByNomProduit(String nom) {
+		return null;
+	}
+
+	@Override
+	public List<Produit> findByNomProduitContains(String nom) {
+		return null;
+	}
+
+
+	@Override
+	public List<Produit> findByNomPrix(String nom, Double prix) {
+		return produitRepository.findByNomPrix(nom, prix);
+	}
+	@Override
+	public List<Produit> findByCategorie(Categorie categorie) {
+		return produitRepository.findByCategorie(categorie);
+	}
+	@Override
+	public List<Produit> findByCategorieIdCat(Long id) {
+		return produitRepository.findByCategorieIdCat(id);
+	}
+
+	@Override
+	public List<Produit> findByOrderByNomProduitAsc() {
+		return null;
+	}
+
+	@Override
+	public List<Produit> trierProduitsNomsPrix() {
+		return null;
+	}
+
 
 }
--- src/test/java/com/example/zeynashop/ZeynaShopApplicationTests.java
+++ src/test/java/com/example/zeynashop/ZeynaShopApplicationTests.java
@@ -4,6 +4,7 @@ package com.example.zeynashop;
 import java.util.Date;
 import java.util.List;
 
+import com.example.zeynashop.entities.Categorie;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -21,7 +22,7 @@ class ProduitsApplicationTests {
     @Test
     //tester ajout d'un nouveau produit
     public void testCreateProduit() {
-        Produit prod = new Produit("HP",4500.500,new Date());
+        Produit prod = new Produit("PS 5",2000.500,new Date());
         produitRepository.save(prod);
     }
     // rechercher un produit par son id
@@ -34,6 +35,8 @@ class ProduitsApplicationTests {
     }
 
 
+
+    // modifier produit
     @Test
     public void testUpdateProduit()
     {
@@ -60,6 +63,69 @@ class ProduitsApplicationTests {
             System.out.println(p);
 
     }
+
+
+    @Test
+    public void tesfindByNomProduitContains()
+    {
+        List<Produit> prods = produitRepository.findAllByNomProduit("hp");
+
+        for (Produit p:prods)
+            System.out.println(p);
+
+    }
+
+    @Test
+    public void tesfindProduitbyNom()
+    {
+        List<Produit> prods = produitRepository.findAllByNomProduit("hp");
+
+        for (Produit p:prods)
+            System.out.println(p);
+
+    }
+
+    @Test
+    public void testTrierProduitsNomsPrix()
+    {
+        List<Produit> prods = produitRepository.TrierProduitsNomsPrix();
+        for (Produit p : prods)
+        {
+            System.out.println(p);
+        }
+    }
+
+    @Test
+    public void testfindByNomPrix()
+    {
+        List<Produit> prods = produitRepository.findByNomPrix("samsun", 1000.0);
+        for (Produit p : prods)
+        {
+            System.out.println(p);
+        }
+    }
+    @Test
+    public void testfindByCategorie()
+    {
+        Categorie cat = new Categorie();
+        cat.setIdCat(1L);
+        List<Produit> prods = produitRepository.findByCategorie(cat);
+        for (Produit p : prods)
+        {
+            System.out.println(p);
+        }
+    }
+
+    @Test
+    public void findByCategorieIdCat()
+    {
+        List<Produit> prods = produitRepository.findByCategorieIdCat(1L);
+        for (Produit p : prods)
+        {
+            System.out.println(p);
+        }
+    }
+
 
 }
 
